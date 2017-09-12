603ccfa1dedab6dc3b4c7956b05cf8968a796744
dean.haines <dean.haines@freetimers.com>
2017-09-12 19:18:53 +0100

    test

---
+++ src/tests/CubicUnitTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * CubicUniTest.php
+ *
+ * @author: Dean Haines
+ * @copyright: Dean Haines, 2017, UK
+ * @license: GPL V3.0+ See LICENSE.md
+ */
+
+namespace vbpupil\tests;
+
+use CubicUniTest;
+use PHPUnit\Framework\TestCase;
+
+class CubicUniTest extends TestCase
+{
+
+}
